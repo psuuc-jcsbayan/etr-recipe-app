65d9bc2a3c33e7a05aacc5e11a987ede8e7ab347
chengslzr2003 <chengslzr2003@gmail.com>
2024-12-08 12:38:52 +0000

    COMMIT2

--- lib/screens/recipe_dashboard.dart
+++ lib/screens/recipe_dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class RecipeApp extends StatefulWidget {
@@ -10,6 +11,65 @@ class RecipeApp extends StatefulWidget {
 class _RecipeAppState extends State<RecipeApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dailycious"),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Expanded(
+                child: SingleChildScrollView(
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                      height: 150.0,
+                      enableInfiniteScroll: true,
+                      autoPlay: true,
+                      autoPlayInterval: Duration(seconds: 5),
+                      autoPlayAnimationDuration: Duration(milliseconds: 500),
+                      pauseAutoPlayOnTouch: true,
+                      enlargeCenterPage: true,
+                    ),
+                    items: [
+                      'assets/images/pang2.jpg',
+                      'assets/images/manaog.jpg',
+                      'assets/images/patar.jpg',
+                      'assets/images/panga1.jpg',
+                      'assets/images/pang3.jpg',
+                      'assets/images/masamirey.jpg',
+                    ].map((imagePath) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return AspectRatio(
+                            aspectRatio: 30 / 9,
+                            child: Card(
+                              color: Color(0xFFFFDE59),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child:
+                                    Image.asset(imagePath, fit: BoxFit.cover),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          //CATEGORIES
+          Divider(),
+          Row(
+            children: [
+              
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
