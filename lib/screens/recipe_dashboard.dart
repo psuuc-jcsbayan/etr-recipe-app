a5660d7578d97307e6256b20cb129080aa057191
chengslzr2003 <chengslzr2003@gmail.com>
2024-12-08 12:27:33 +0000

    commit

---
+++ lib/screens/recipe_dashboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecipeApp extends StatefulWidget {
+  const RecipeApp({super.key});
+
+  @override
+  State<RecipeApp> createState() => _RecipeAppState();
+}
+
+class _RecipeAppState extends State<RecipeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
